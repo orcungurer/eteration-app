f6e5906b3b9a724bdd3249cb5ac68608d7386443
orcungurer <orcungurer@hotmail.com>
2023-05-03 02:23:07 +0300

    added searchbar feature

---
+++ src/components/filter/SearchBar.js
@@ -0,0 +1,28 @@
+import { useDispatch, useSelector } from "react-redux";
+import { filterActions } from "../../store/filter-slice";
+import SearchBarIcon from "../icons/SearchBarIcon";
+import classes from "./SearchBar.module.css";
+
+const SearchBar = () => {
+  const dispatch = useDispatch();
+  const searchbar = useSelector((state) => state.filter.searchbar);
+
+  const inputChangeHandler = (event) => {
+    dispatch(filterActions.setSearchbar(event.target.value));
+  };
+
+  return (
+    <div className={classes.searchbar}>
+      <label htmlFor="search"><SearchBarIcon /></label>
+      <input
+        type="text"
+        id="search"
+        placeholder="Search"
+        value={searchbar}
+        onChange={inputChangeHandler}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,12 @@
 import CartButton from "../cart/CartButton";
+import SearchBar from "../filter/SearchBar";
 import classes from "./Header.module.css";
 
 const Header = () => {
   return (
     <header className={classes.header}>
       <h1>eteration</h1>
-      <h2>search bar</h2>
+      <SearchBar />
       <nav>
         <ul>
           <li>
