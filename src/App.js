6ede04c455585f98a6e964fed8be51fbe9b296f4
orcungurer <orcungurer@hotmail.com>
2023-05-03 02:25:25 +0300

    completed app component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
+import { Fragment, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Cart from "./components/cart/Cart";
+import Filter from "./components/filter/Filter";
+import Layout from "./components/layout/Layout";
+import ProductDetail from "./components/products/ProductDetail";
+import Products from "./components/products/Products";
+import { cartActions } from "./store/cart-slice";
+import { fetchProductData } from "./store/product-slice";
+
 function App() {
-  return <h1>Hello World</h1>;
+  const dispatch = useDispatch();
+  const showProductDetails = useSelector((state) => state.ui.productDetails);
+
+  useEffect(() => {
+    dispatch(fetchProductData());
+
+    // local storage
+    const cartData = JSON.parse(localStorage.getItem('cartItems'));
+    dispatch(cartActions.replaceCart({
+      items: cartData.items || [],
+      totalPrice: cartData.totalPrice,
+      totalQuantity: cartData.totalQuantity,
+    }));
+  }, [dispatch]);
+
+  return (
+    <Layout>
+      {!showProductDetails && (
+        <Fragment>
+          <Filter />
+          <Products />
+        </Fragment>
+      )}
+      {showProductDetails && (
+        <ProductDetail />
+      )}
+      <Cart />
+    </Layout>
+  );
 }
 
 export default App;
